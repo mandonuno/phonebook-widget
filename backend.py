b599cc9a2581eececd94d24c732f2af7ca5bd323
mandonuno <mandonuno94@gmail.com>
2018-10-29 12:37:56 -0700

    PEP8 compliant

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 class Database:
     """Database Class which stores function calls
     to connect, insert, view, search, and delete items
@@ -9,12 +10,18 @@ class Database:
         """Connects and creates the contacts db"""
         self.conn = sqlite3.connect(db)
         self.cur = self.conn.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS phonebook (id INTEGER PRIMARY KEY, first text, last text, cell integer, email text)")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS\
+                          phonebook (id INTEGER PRIMARY KEY,\
+                          first text,\
+                          last text,\
+                          cell integer,\
+                          email text)")
         self.conn.commit()
 
     def insert(self, first, last, cell, email):
         """Inserts data into contacts db"""
-        self.cur.execute("INSERT INTO phonebook VALUES (NULL,?,?,?,?)", (first,last,cell,email))
+        self.cur.execute("INSERT INTO phonebook VALUES (NULL, ?, ?, ?, ?)",
+                         (first, last, cell, email))
         self.conn.commit()
 
     def view(self):
@@ -25,7 +32,9 @@ class Database:
 
     def search(self, first="", last="", cell="", email=""):
         """Searches database for given contact"""
-        self.cur.execute("SELECT * FROM phonebook WHERE first=? OR last=? OR cell=? OR email=?", (first,last,cell,email))
+        self.cur.execute("SELECT * FROM phonebook\
+                          WHERE first=? OR last=? OR cell=? OR email=?",
+                         (first, last, cell, email))
         rows = self.cur.fetchall()
         return rows
 
@@ -36,10 +45,11 @@ class Database:
 
     def update(self, id, first, last, cell, email):
         """Updates database entries"""
-        self.cur.execute("UPDATE phonebook SET first=?, last=?, cell=?, email=? WHERE id=?", (first,last,cell,email,id))
+        self.cur.execute("UPDATE phonebook\
+                          SET first=?, last=?, cell=?, email=? WHERE id=?",
+                         (first, last, cell, email, id))
         self.conn.commit()
-    
+
     def __del__(self):
         """Closes connection to database"""
         self.conn.close()
-
